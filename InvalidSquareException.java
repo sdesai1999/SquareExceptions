be39211b1bebcf51bbb469a7a5f90311b4597e73
Saurav Desai <sauravdesai@gmail.com>
2017-10-22 03:12:44 -0400

    added javadocs and made minor edits, still need to make edits

--- InvalidSquareException.java
+++ InvalidSquareException.java
@@ -1,5 +1,20 @@
+/**
+ * Extends the Exception class; will be thrown if an impossible square is
+ * attempted to be created.
+ * This exception is a checked exception, because I added a throws clause in the
+ * Square constructors and I use a try catch block in my Tester class.
+ *
+ * @author sdesai88
+ * @version 10/24/17
+*/
 public class InvalidSquareException extends Exception {
 
+    /**
+     * Creates an InvalidSquareException by calling the constructor in the
+     * Exception class (parent class).
+     *
+     * @param square : the name of the invalid Square
+    */
     public InvalidSquareException(String square) {
         super(square);
     }
--- Tester.java
+++ Tester.java
@@ -1,5 +1,5 @@
 public class Tester {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InvalidSquareException {
         try {
             Square a9 = new Square('a', '9');
             System.out.println("WORKS");
@@ -10,7 +10,13 @@ public class Tester {
         try {
             Square a3 = new Square("a3");
             System.out.println("WORKS");
-            System.out.println(a3);
+        } catch (InvalidSquareException e) {
+            System.out.printf("ERROR: %s%n", e.getMessage());
+        }
+
+        try {
+            new Square("j3");
+            System.out.println("WORKS");
         } catch (InvalidSquareException e) {
             System.out.printf("ERROR: %s%n", e.getMessage());
         }
